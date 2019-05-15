4cb3686460be3c4e78688987683890ec1f86f926
Dev Davis <sean.dev.davis@gmail.com>
2019-05-14 19:24:13 -0500

    wip

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -1,9 +1,25 @@
 import boardData from '../../helpers/boardsData';
+import print from '../../helpers/util';
+
+
+const domStringBuilder = (boards) => {
+  let domString = '';
+
+  domString += '<div class="row">';
+  boards.forEach((board) => {
+    domString += '<div class="col-3">';
+    domString += `<p>${board.name}</p>`;
+    domString += '</div>';
+  });
+  domString += '</div>';
+  print.printToDom('user-boards', domString);
+};
 
 const initBoards = () => {
-  boardData.leadBoards()
+  boardData.loadBoards()
     .then((resp) => {
       console.error('resp', resp.data.boards);
+      domStringBuilder(resp.data.boards);
     })
     .catch(err => console.error('error from loadBoards', err));
 };
